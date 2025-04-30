51720e9750688fb251aac9701c9e368dfb88557c
user <kierhagos0@gmail.com>
2025-04-30 10:52:03 +0800

    chore: integrate NativeWind

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,9 @@
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 export default function HomeScreen() {
-  return <View></View>;
+  return (
+    <View>
+      <Text className="text-green-400">Home Screen</Text>
+    </View>
+  );
 }
